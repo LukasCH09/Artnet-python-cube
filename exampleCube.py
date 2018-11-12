e521b5333d44a17f75c9e387db587c9e330f7c44
lukas.bitter <lukas.bitter@he-arc.ch>
2018-11-12 22:24:22 +0100

    add command line arguments

--- exampleCube.py
+++ exampleCube.py
@@ -1,60 +1,101 @@
+import sys
+
 from lib.primitives import CubeAPI
 from lib.StupidArtnet import StupidArtnet
 import time
 
-target_ip = '127.0.0.1'	 # typically in 2.x or 10.x range
-universe = 0 					# see docs
-packet_size = 512				# it is not necessary to send whole universe
+target_ip = '127.0.0.1'  # typically in 2.x or 10.x range
+universe = 0  # see docs
+packet_size = 512  # it is not necessary to send whole universe
+allowed_indices = [i for i in range(0, 9)] + \
+                  [i for i in range(100, 109)]
 
-# CREATING A STUPID ARTNET OBJECT
-# SETUP NEEDS A FEW ELEMENTS
-# TARGET_IP   = DEFAULT 127.0.0.1
-# UNIVERSE    = DEFAULT 0
-# PACKET_SIZE = DEFAULT 512
-# FRAME_RATE  = DEFAULT 30
-a = StupidArtnet(target_ip, universe, packet_size)
 
-a.set_simplified(False)
-# MORE ADVANCED CAN BE SET WITH SETTERS IF NEEDED
-# NET         = DEFAULT 0
-# SUBNET      = DEFAULT 0
+def send_packets(_vertical, artnet):
+    packet = bytearray(packet_size)  # create packet for Artnet
+    brightness = 255
+    for key in _vertical:
+        print(f'Universe: {key} => channels: {_vertical[key]}')
+        for i in range(packet_size):  # fill packet with sequential values
+            packet[i] = brightness if i + 1 in _vertical[key] else 0
+        subnet = int(key / 16)
+        universe = key % 16
+        print(f'subnet: {subnet}, universe: {universe}, packet: {packet}')
+        artnet.set_subnet(subnet)
+        artnet.set_universe(universe)
+        artnet.set(packet)  # set the packet to stupid Artnet
+        print(artnet)
+        artnet.show()  # send the data
 
-# CHECK INIT
-print(a)
 
-a.start()       # start continuous sending, else error pops when cleaning
+def turn_off(_vertical, artnet):
+    # SOME DEVICES WOULD HOLD LAST DATA, TURN ALL OFF WHEN DONE
+    for key in _vertical:
+        subnet = int(key / 16)
+        universe = key % 16
+        print(f'blackout subnet {subnet} universe {universe}')
+        artnet.set_subnet(subnet)
+        artnet.set_universe(universe)
+        artnet.blackout()
 
-packet = bytearray(packet_size)		# create packet for Artnet
-brightness = 255
 
-cubeAPI = CubeAPI()
-vertical = cubeAPI.get_vertical(0)  # set the indice of vertical bar (0 to 8)
-for key in vertical:
-    print(f'Universe: {key} => channels: {vertical[key]}')
-    for i in range(packet_size):  # fill packet with sequential values
-        packet[i] = brightness if i+1 in vertical[key] else 0
-    subnet = int(key / 16)
-    universe = key % 16
-    print(f'subnet: {subnet}, universe: {universe}, packet: {packet}')
-    a.set_subnet(subnet)
-    a.set_universe(universe)
-    a.set(packet)           # set the packet to stupid Artnet
+def main(param):
+    indice = param[0]
+
+    if len(param) > 1:
+        sleep_time = param[1]
+    else:
+        sleep_time = 5
+
+    # CREATING A STUPID ARTNET OBJECT
+    # SETUP NEEDS A FEW ELEMENTS
+    # TARGET_IP   = DEFAULT 127.0.0.1
+    # UNIVERSE    = DEFAULT 0
+    # PACKET_SIZE = DEFAULT 512
+    # FRAME_RATE  = DEFAULT 30
+    a = StupidArtnet(target_ip, universe, packet_size)
+
+    a.set_simplified(False)
+    # MORE ADVANCED CAN BE SET WITH SETTERS IF NEEDED
+    # NET         = DEFAULT 0
+    # SUBNET      = DEFAULT 0
+
+    # CHECK INIT
     print(a)
-    a.show()                # send the data
 
-time.sleep(2)
+    a.start()  # start continuous sending, else error pops when cleaning
 
-# SOME DEVICES WOULD HOLD LAST DATA, TURN ALL OFF WHEN DONE
-for key in vertical:
-    subnet = int(key / 16)
-    universe = key % 16
-    print(f'blackout subnet {subnet} universe {universe}')
-    a.set_subnet(subnet)
-    a.set_universe(universe)
-    a.blackout()
+    cubeAPI = CubeAPI()
+    vertical = cubeAPI.get_vertical(indice)  # set the indice of vertical bar (0 to 8)
 
-# ... REMEMBER TO CLOSE THE THREAD ONCE YOU ARE DONE
-a.stop()
+    send_packets(vertical, a)
+    time.sleep(sleep_time)
+    turn_off(vertical, a)
 
-# CLEANUP IN THE END
-del a
+    # ... REMEMBER TO CLOSE THE THREAD ONCE YOU ARE DONE
+    a.stop()
+
+    # CLEANUP IN THE END
+    del a
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print("len(sys.argv)" + str(len(sys.argv)))
+        main(sys.argv[1:])
+    else:
+        indice_needed = True
+        number = 0
+        while indice_needed:
+            print(f"Allowed indices: {allowed_indices}")
+            ind = input('Enter vertical indice :')
+            try:
+                number = int(ind)
+            except ValueError:
+                print("Invalid number")
+            if number not in allowed_indices:
+                print(f"Unallowed indice. Allowed indices are: "
+                      f"{allowed_indices}")
+            else:
+                indice_needed = False
+        main([number])
